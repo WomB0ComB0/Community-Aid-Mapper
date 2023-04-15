72165d197c14e5dc0755beb6d5d7e3197312a490
Mike Odnis <mikeodnis3242004@gmail.com>
2023-04-15 16:30:01 -0400

    Slight mods

--- Community-Aid-Mapper-App/src/App.tsx
+++ Community-Aid-Mapper-App/src/App.tsx
@@ -1,13 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import NotFound from './Components/404'
+import {FallBack as Offline} from './Components/fallback'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+  const [isLoading, setIsLoading] = useState(true);
 
-  return ( 
+  useEffect(() =>{
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
+
+    return () => {
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
+    };
+
+  },[])
+  const handleOnline = () => { setIsOnline(true); };
+  const handleOffline = () => { setIsOnline(false); };
+  return (
     <>
-    Hello World
+      {!isOnline ? 
+        <Offline/>: 
+        <Router>
+            <Routes>
+              <Route path="*" element={<NotFound></NotFound>}/>
+            </Routes>
+          </Router>
+      }
     </>
-  )
-}
-
+  );
+};
 export default App
---
+++ Community-Aid-Mapper-App/src/Components/404.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className="">
+      <div>
+        404
+      </div>
+    </div>
+  )
+}
+export default NotFound;
