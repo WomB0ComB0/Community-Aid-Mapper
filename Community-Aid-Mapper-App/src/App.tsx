5fd888ac835132a8d030fc691bbfac883ffbed3e
Mike Odnis <mikeodnis3242004@gmail.com>
2023-04-15 17:15:21 -0400

    File structure and other things

--- Community-Aid-Mapper-App/src/App.tsx
+++ Community-Aid-Mapper-App/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import NotFound from './Components/404'
-import {FallBack as Offline} from './Components/fallback'
+import NotFound from './Flow/Misc/404'
+import {FallBack as Offline} from './Flow/Misc/fallback'
 
 const App = () => {
   const [isOnline, setIsOnline] = useState(navigator.onLine);
