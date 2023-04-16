55a57ccb37dcc5190bb12cc8afeef5df77dec033
Mike Odnis <mikeodnis3242004@gmail.com>
2023-04-16 14:46:24 -0400

    Adding things

--- Community-Aid-Mapper-App/src/main.tsx
+++ Community-Aid-Mapper-App/src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {Routes, Route} from 'react-router-dom'
 import App from './App'
+import Sidebar from './sidebar/SideBar';
 import './assets/Styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/" element={<Sidebar/>}/>
+    </Routes>
   </React.StrictMode>,
 )
